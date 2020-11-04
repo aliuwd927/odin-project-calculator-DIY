fa4bd9d98ec753f966ee1e7a844ee4e91846706e
Andy Liu <aliu927@gmail.com>
2020-11-03 21:04:59 -0800

    commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const displayInput = document.querySelector('.displayInput');
+const displayOutput = document.querySelector('.displayOutput');
+const keyInputs = document.querySelector('.keyInput');
+
+keyInputs.addEventListener('click', (e)=>{
+    console.log(e.target);
+});
