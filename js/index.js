aa527d61989907cbc23c7a0180c07ada33f59bc9
Andy Liu <aliu927@gmail.com>
2020-11-04 00:34:52 -0800

    update js

--- js/index.js
+++ js/index.js
@@ -2,6 +2,19 @@ const displayInput = document.querySelector('.displayInput');
 const displayOutput = document.querySelector('.displayOutput');
 const keyInputs = document.querySelector('.keyInput');
 
+let dataInput = [];
+
 keyInputs.addEventListener('click', (e)=>{
-    console.log(e.target);
+    //console.log(e);
+    
+    if(e.target.classList.contains('number')){
+        dataInput.push(e.target.textContent);
+        displayOutput.textContent = dataInput.join('');
+    }else if(e.target.classList.contains('operator')){
+        dataInput.push(e.target.textContent);
+        displayOutput.textContent = dataInput.join('');
+    }else if(e.target.classList.contains('key')){
+        
+    }
+    console.log(dataInput);
 });
